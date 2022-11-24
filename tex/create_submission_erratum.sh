fdfdab728d92a80561dcf7eb68471f522c15bb30
Alexei Vazquez <alexei@nodeslinks.com>
2022-11-24 13:23:53 +0100

    errata files

---
+++ tex/create_submission_erratum.sh
@@ -0,0 +1,1 @@
+zip erratum.v1.zip errata.tex maxsum.scheduling.dupsplit.arc_distribution_zero.eps maxsum.scheduling.dupsplit.arc_distribution_lognormal.arc_sigma_1.eps maxsum.scheduling.dupsplit.arc_distribution_lognormal.arc_sigma_3.eps
