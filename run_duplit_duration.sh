475dc18e4615523ce33e4b89482823459c068cc6
av2atgh <alexei@nodeslinks.com>
2023-12-08 15:29:51 +0100

    add runner

---
+++ run_duplit_duration.sh
@@ -0,0 +1,7 @@
+./dupsplit_generation -n_nodes 1000000 -duplication_rate 0.1 -n_nets 10 -dmean 30
+./dupsplit_generation -n_nodes 1000000 -duplication_rate 0.2 -n_nets 10 -dmean 30
+./dupsplit_generation -n_nodes 1000000 -duplication_rate 0.3 -n_nets 10 -dmean 30
+./dupsplit_generation -n_nodes 1000000 -duplication_rate 0.4 -n_nets 10 -dmean 30
+./dupsplit_generation -n_nodes 1000000 -duplication_rate 0.5 -n_nets 10 -dmean 30
+./dupsplit_generation -n_nodes 1000000 -duplication_rate 0.6 -n_nets 10 -dmean 30
+
